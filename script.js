80c3bc2287ca1baad8d1052341de98df56e6e026
sanjeevi2005 <ssanjeevi884@gmail.com>
2025-10-06 19:45:37 +0530

    Code Updated Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var offerBoxclose = document.querySelector(".offer_box i");
+    offerBoxclose.addEventListener("click", function() {
+        var offerBox = document.querySelector(".offer_box");
+        offerBox.style.display = "none";
+    });
+
+    var navbarSideActions = document.querySelector(".navbar-side-actions");
+    var navbarSideMenu = document.querySelector(".navbar_side-links");
+    var closeBtn = document.querySelector(".navbar_side-links .close-btn");
+    var navbar = document.querySelector(".navbar");
+    navbarSideActions.addEventListener("click", function() {
+        navbarSideMenu.style.display = "flex";
+        navbarSideMenu.style.zIndex = "1000";
+        navbar.style.display = "none";
+
+    });
+    closeBtn.addEventListener("click", function() {
+        navbarSideMenu.style.display = "none";
+        navbar.style.display = "flex";
+    });
+
+    const imageContainer = document.querySelector(".slider-image-container");
+    const sliderLeftButton = document.getElementById("slider-left-activate");
+    const sliderRightButton = document.getElementById("slider-right-activate");
+    
+    if (imageContainer && sliderLeftButton && sliderRightButton) {
+        const totalImages = document.querySelectorAll(".slider-image").length;
+        let currentMargin = 0;
+        const maxMargin = (totalImages - 1) * 100; 
+
+        sliderRightButton.addEventListener("click", function () {
+            currentMargin += 100;
+            if (currentMargin > maxMargin) {
+                currentMargin = 0;
+            }
+            imageContainer.style.marginLeft = `-${currentMargin}vw`;
+        });
+         
+        sliderLeftButton.addEventListener("click", function () {  
+            currentMargin -= 100;
+            if (currentMargin < 0) {
+                currentMargin = maxMargin;
+            }
+            imageContainer.style.marginLeft = `-${currentMargin}vw`;
+        });
+    }
+});
